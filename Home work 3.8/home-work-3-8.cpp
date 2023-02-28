8c2711ca3f894c897a0d0332ecf24e239f720b8b
studys22 <125546285+studys22@users.noreply.github.com>
2023-02-28 17:55:19 +0300

    Create home-work-3-8.cpp

---
+++ Home work 3.8/home-work-3-8.cpp
@@ -0,0 +1,29 @@
+﻿// https://github.com/studys22/Synergy-Programming-technology
+
+#include <locale.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main() {
+	setlocale(LC_ALL, "ru-RU");
+
+	int n, sum = 0, min = INT_MAX, max = INT_MIN, a;
+	printf("Количество элементов: ");
+	scanf_s("%i", &n);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%i", &a);
+		sum += a;
+		if (min > a) {
+			min = a;
+		}
+		if (max < a) {
+			max = a;
+		}
+	}
+	float avg = (float)sum / (float)n;
+
+	printf("Минимальное: %i\n", min);
+	printf("Максимальное: %i\n", max);
+	printf("Сумма: %i\n", sum);
+	printf("Среднее: %f\n", avg);
+}
