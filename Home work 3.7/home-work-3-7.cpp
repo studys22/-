8d8b0dc6aaed32d4d09ba01b21d268ce7e6a5704
studys22 <125546285+studys22@users.noreply.github.com>
2023-02-28 17:40:59 +0300

    Create home-work-3-7.cpp

---
+++ Home work 3.7/home-work-3-7.cpp
@@ -0,0 +1,20 @@
+﻿// https://github.com/studys22/Synergy-Programming-technology
+
+#include <locale.h>
+#include <stdio.h>
+
+int summa(int a1, int an, int d) {
+	return ((a1 + an) * (an - a1) / d + a1 + an) / 2;
+}
+
+int main() {
+	setlocale(LC_ALL, "ru-RU");
+
+	int a1 = 1, an = 100, d = 1;
+	int s = 0, s_etalon = summa(a1, an, d);
+	for (int i = a1; i <= an; i += d) {
+		s += i;
+	}
+	printf("Сумма посчитанная в цикле: %i\n", s);
+	printf("Сумма посчитанная через формулу арифметической прогрессии: %i\n", s_etalon);
+}
