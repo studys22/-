3d900c22db246e5ac66f34882e1438176bd8e175
studys22 <125546285+studys22@users.noreply.github.com>
2023-02-28 11:24:22 +0300

    Home work 3-6

---
+++ Home work 3.6/home-work-3-6-1.cpp
@@ -0,0 +1,9 @@
+﻿// https://github.com/studys22/Synergy-Programming-technology
+
+#include <stdio.h>
+
+int main() {
+	for (int i = 1; i <= 10; i++) {
+		printf("%i * 5 = %i\n", i, i*5);
+	}
+}
---
+++ Home work 3.6/home-work-3-6-2.cpp
@@ -0,0 +1,13 @@
+﻿// https://github.com/studys22/Synergy-Programming-technology
+
+#include <stdio.h>
+
+int main() {
+	int a;
+	for(scanf_s("%i", &a); a != 7; scanf_s("%i", &a)) {
+		printf("%i %c 7\n",  a, a < 7  ? '<' : '>');
+		printf("%i %s 10\n", a, a < 10 ? "<" : (a > 10 ? ">" : "=="));
+		printf("%i %% 2 %c= 0\n",   a, a % 2 ? '!' : '=');
+		printf("%i %% 3 %c= 0\n\n", a, a % 3 ? '!' : '=');
+	}
+}
