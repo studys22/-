24394a4ab252a0ff93d997a3d1fea30feecef629
studys22 <125546285+studys22@users.noreply.github.com>
2023-03-15 12:51:14 +0300

    Add home work 5.2

---
+++ Home work 5.2/home-work-5-2.cpp
@@ -0,0 +1,25 @@
+﻿// https://github.com/studys22/Synergy-Programming-technology
+
+#include <locale.h>
+#include <stdio.h>
+
+int main() {
+	setlocale(LC_ALL, "ru-RU");
+
+	int a[100][100], n, m;
+	printf("Размерность массива: ");
+	scanf_s("%i %i", &n, &m);
+	printf("Введите числа: ");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf_s("%i", &a[i][j]);
+		}
+	}
+	printf("Введенный массив:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%4i", a[i][j]);
+		}
+		printf("\n");
+	}
+}
