ccfb38f5368a1b4d063a014ebdce0589534a8e3c
studys22 <125546285+studys22@users.noreply.github.com>
2023-03-15 12:51:41 +0300

    Add home work 5.3

---
+++ Home work 5.3/home-work-5-3.cpp
@@ -0,0 +1,27 @@
+﻿// https://github.com/studys22/Synergy-Programming-technology
+
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	setlocale(LC_ALL, "ru-RU");
+
+	int a[100][100], n, m;
+	printf("Размерность массива: ");
+	scanf_s("%i %i", &n, &m);
+	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			a[i][j] = rand() % 10 - 2;
+		}
+	}
+	printf("Введенный массив:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%4i", a[i][j]);
+		}
+		printf("\n");
+	}
+}
