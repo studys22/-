4566388f59a9c78dd713824a67e5a0c4adbc6653
studys22 <125546285+studys22@users.noreply.github.com>
2023-03-31 21:55:42 +0300

    Intermediate commit

--- Home work 8.1/home-work-8-1-1.cpp
+++ Home work 8.1/home-work-8-1-1.cpp
@@ -2,6 +2,7 @@
 
 #include <locale.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 enum Sex {
@@ -10,16 +11,20 @@ enum Sex {
 };
 
 class Person {
-protected:
+ protected:
     char* name;
     unsigned char age, weight;
     Sex sex;
 public:
+    void setSex(Sex _sex) {
+       sex = _sex;
+    }
     void setName(const char* name_) {
+        name = (char*)malloc(strlen(name_) + 1);
         strcpy(name, name_);
     }
     char* getName() {
-        char* name_;
+        char* name_= (char*)malloc(strlen(name) + 1);
         strcpy(name_, name);
         return name_;
     }
@@ -31,14 +36,41 @@ public:
     }
 };
 
-class Student : Person {
+class Student : public Person {
 protected:
     unsigned char age_on_start_education;
-
+    
+public:
+    char* afterYear() {
+        age++;
+        weight += rand() % 10 - 5;
+        char* res;
+        strcat(res, name);
+        strcat(res, ". Age: ");
+        char t[3];
+        strcat(res, itoa(age, t, 10));
+        strcat(res, ". Weight: ");
+        strcat(res, itoa(weight, t, 10));
+        strcat(res, ". Sex: ");
+        strcat(res, sex == male ? "male" : "female");
+        return res;  
+    }
 };
 
 int main() {
 	setlocale(LC_ALL, "ru-RU");
 
-	
+    srand(NULL);
+
+    Student s;
+    s.setSex(male);
+    s.setName("Иванов Иван");
+    s.setAge(18);
+    s.setWeight(70);
+
+    char* t = s.afterYear();
+
+    printf("%s\n", t);
+    printf("%s\n", s.afterYear());
+    printf("%s\n", s.afterYear());	
 }
