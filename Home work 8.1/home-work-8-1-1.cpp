4715823fb297a3c6d1b1ea7c03be1a7851d0ee2c
studys22 <125546285+studys22@users.noreply.github.com>
2023-04-01 12:03:12 +0300

    home-work-8-1-1

--- Home work 8.1/home-work-8-1-1.cpp
+++ Home work 8.1/home-work-8-1-1.cpp
@@ -4,20 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 char* itoa_(int val, int base){
 	
-	static char buf[32] = {0};
-	
+	static char buf[32] = {0};	
 	int i = 30;
 	
-	for(; val && i ; --i, val /= base)
-	
+	for(; val && i ; --i, val /= base)	
 		buf[i] = "0123456789abcdef"[val % base];
 	
 	return &buf[i+1];
 	
 }
+
 enum Sex {
     male,
     female
@@ -57,7 +57,12 @@ public:
     char* afterYear() {
         age++;
         weight += rand() % 10 - 5;
-        char* res = (char*)malloc(101);
+        int v = rand() % 100;
+        if (v >= 95) {
+            sex = sex == male ? female : male;
+        }
+        char* res = new char[sizeof(char)*100];
+        res[0] = '\0';
         strcat(res, name);
         strcat(res, ". Age: ");
         strcat(res, itoa_(age, 10));
@@ -72,7 +77,7 @@ public:
 int main() {
 	setlocale(LC_ALL, "ru-RU");
 
-    srand(NULL);
+    srand(time(NULL));
 
     Student s;
     s.setSex(male);
@@ -80,9 +85,7 @@ int main() {
     s.setAge(18);
     s.setWeight(70);
 
-    char* t = s.afterYear();
-
-    printf("%s\n", t);
-    printf("%s\n", s.afterYear());
-    printf("%s\n", s.afterYear());	
+    for (int i = 0; i < 6; i++) {
+        printf("After year %i: %s\n", i + 1, s.afterYear());
+    }	
 }
